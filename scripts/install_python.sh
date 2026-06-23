cc476b38148869c3a5ea522d7fca8e964e05703f
ornel-Am <amoussouornel1@gmail.com>
2026-06-23 16:12:09 +0000

    Delete scripts directory

--- scripts/install_python.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
-mkdir -p "$PROJECT_DIR/portable_python"
-tar -xzf "$PROJECT_DIR/deps/linux/python-portable.tar.gz" -C "$PROJECT_DIR/portable_python" --strip-components=1
-"$PROJECT_DIR/portable_python/bin/python3" -m pip install --no-index --find-links="$PROJECT_DIR/deps/wheels" pyinstaller
-echo "✅ Python portable installé."
